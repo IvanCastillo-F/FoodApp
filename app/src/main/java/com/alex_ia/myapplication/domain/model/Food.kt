faa823f08cabaa88fc63eb85d6ac3a15f2319f65
Ivan <alex9012345678@gmail.com>
2021-10-20 19:06:46 -0500

    main activity y foodfragment
    Se agregaron elementos visuales,se agrego una navbar en el activity main y se creo la vista de las comidas.

---
+++ app/src/main/java/com/alex_ia/myapplication/domain/model/Food.kt
@@ -0,0 +1,27 @@
+package com.alex_ia.myapplication.domain.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+@Entity
+@JsonClass(generateAdapter = true)
+class Food (
+    @PrimaryKey(autoGenerate = false)
+    @Json(name = "idMeal") val idFood: Int = 0,
+    @Json(name = "strMeal") val name: String = "",
+    @Json(name = "strCategory") val category: String = "",
+    @Json(name = "strMealThumb") val urlThumb: String = "",
+    @Json(name = "strImageSource") val url: String? = "",
+    @Json(name = "strArea") val area: String? = "",
+    @Json(name = "strInstructions") val instructions: String? = ""
+) : Parcelable {
+
+    val urlDetail: String
+        get() = url ?: urlThumb
+
+}
---
+++ app/src/main/java/com/alex_ia/myapplication/presentation/food/FoodViewModel.kt
@@ -0,0 +1,7 @@
+package com.alex_ia.myapplication.presentation.food
+
+import androidx.lifecycle.ViewModel
+
+class FoodViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
